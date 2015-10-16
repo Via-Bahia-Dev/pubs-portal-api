d3190c530d91df6ae57cf35bfeef47a030d7b168
Nathan Connor <nconnor@leapfrog.com>
2015-10-16 10:38:14 -0700

    Changed json response to be standardized

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,20 +4,20 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    render json: @users
+    render json: { data: @users }
   end
 
   def show
-    render json: @user
+    render json: { data: @user }
   end
 
   def create
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: { data: @user }, status: :created, location: @user
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: { errors: @user.errors }, status: :unprocessable_entity
     end
   end
 
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
     if @user.update(user_params)
       head :no_content
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: { errors: @user.errors }, status: :unprocessable_entity
     end
   end
 
