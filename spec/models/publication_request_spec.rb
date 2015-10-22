5a1ae0e5f8fa12342b05c614cc6ac1bb9afc8182
David Wosk <dwosk@asperasoft.com>
2015-10-22 16:03:52 -0700

    added publication_request model spec

--- spec/models/publication_request_spec.rb
+++ spec/models/publication_request_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe PublicationRequest, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:publication_request) { PublicationRequest.create(event: "PR model spec", description: "this is fun.",  rough_date: "Mon, 17 Dec 2015 00:00:00 +0000", due_date: "Mon, 17 Dec 2015 00:00:00 +0000", event_date: "Mon, 17 Dec 2015 00:00:00 +0000", dimensions: "quarter", user_id: 1 ) }
+
+  describe "db structure" do
+    it { is_expected.to have_db_column(:event).of_type(:string) }
+    it { is_expected.to have_db_column(:description).of_type(:text) }
+    it { is_expected.to have_db_column(:rough_date).of_type(:datetime) }
+    it { is_expected.to have_db_column(:due_date).of_type(:datetime) }
+    it { is_expected.to have_db_column(:event_date).of_type(:datetime) }
+    it { is_expected.to have_db_column(:dimensions).of_type(:string) }
+    it { is_expected.to have_db_column(:user_id).of_type(:integer) }
+  end
+
+  describe "associations" do
+    it { is_expected.to belong_to(:user) }
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:event) }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:rough_date) }
+    it { is_expected.to validate_presence_of(:due_date) }
+    it { is_expected.to validate_presence_of(:event_date) }
+    it { is_expected.to validate_presence_of(:dimensions) }
+    it { is_expected.to validate_presence_of(:user_id) }
+  end
+
 end
