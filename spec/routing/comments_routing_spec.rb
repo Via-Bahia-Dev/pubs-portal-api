6890f03d0e68961d370f16922a29979a1a419cae
David Wosk <dwosk@asperasoft.com>
2015-10-23 15:52:13 -0700

    comments for publication requests and specs

---
+++ spec/routing/comments_routing_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe CommentsController, type: :routing do
+  it { expect(get:    "/comments").to   route_to("comments#index") }
+  it { expect(get:    "/comments/1").to route_to("comments#show", id: "1") }
+  it { expect(post:   "/comments").to   route_to("comments#create") }
+  it { expect(put:    "/comments/1").to route_to("comments#update", id: "1") }
+  it { expect(delete: "/comments/1").to route_to("comments#destroy", id: "1") }
+end
