c861a3356e2bc5c7c9052a277894a9bb1c65377e
Nathan Connor <nconnor@leapfrog.com>
2015-12-03 11:46:27 -0800

    Updated spec and controller with category column

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -37,7 +37,7 @@ class TemplatesController < ApplicationController
   private
 
   def template_params
-    params.require(:template).permit(:name, :user_id, :dimensions, :image, :link, :type)
+    params.require(:template).permit(:name, :user_id, :dimensions, :image, :link, :category)
   end
 
 end
