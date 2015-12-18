6a986168520ce3c513e9a9db0a6fbc75747b4a52
Nathan Connor <nconnor@leapfrog.com>
2015-12-18 11:21:14 -0800

    Serialized Request Attachments

---
+++ app/serializers/request_attachment_serializer.rb
@@ -0,0 +1,26 @@
+class RequestAttachmentSerializer < ActiveModel::Serializer
+  attributes :id, :created_at, :updated_at, :comment,
+  								:file_file_name, :file_file_size, :file_original, :file_large, :file_medium, :file_small, :file_thumb
+
+  belongs_to :user
+
+  def file_original
+  	object.file.url
+  end
+
+  def file_large
+  	object.file.url(:large)
+  end
+
+  def file_medium
+  	object.file.url(:medium)
+  end
+
+  def file_small
+  	object.file.url(:small)
+  end
+
+  def file_thumb
+  	object.file.url(:thumb)
+  end
+end
