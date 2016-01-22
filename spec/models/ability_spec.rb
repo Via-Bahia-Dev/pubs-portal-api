709b1ff65ba4cb36aae0f3b846240375f6372379
Nathan Connor <natyconnor@gmail.com>
2016-01-21 16:22:27 -0800

    Updated ability spec for new request abilities

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -22,11 +22,12 @@ RSpec.describe Ability, type: :model do
 			}
 
 			context "when is an admin" do
-				
-
-				it "can manage Users" do
+				it "can manage all models" do
 					user.roles = [:admin, :user]
 					should be_able_to(:manage, User)
+					should be_able_to(:manage, PublicationRequest)
+					should be_able_to(:manage, Comment)
+					should be_able_to(:manage, RequestAttachment)
 				end
 			end
 
@@ -36,38 +37,75 @@ RSpec.describe Ability, type: :model do
 				# User
 				it { should_not be_able_to(:create, User) }
 				it { should_not be_able_to(:index, User) }
-				it { should be_able_to(:show, user) }
+				it { should     be_able_to(:show, user) }
 				it { should_not be_able_to(:update, User.new) }
-				it { should be_able_to(:update, user)}
+				it { should     be_able_to(:update, user)}
 				it { should_not be_able_to(:delete, User) }
 
 				# Request Attachment
-				it { should be_able_to(:read, RequestAttachment) }
-				it { should be_able_to(:create, RequestAttachment) }
-				it { should be_able_to(:update, attachment) }
+				it { should     be_able_to(:read, RequestAttachment) }
+				it { should     be_able_to(:create, RequestAttachment) }
+				it { should     be_able_to(:update, attachment) }
 				it { should_not be_able_to(:update, RequestAttachment.new) }
-				it { should be_able_to(:destroy, attachment) }
+				it { should     be_able_to(:destroy, attachment) }
 				it { should_not be_able_to(:destroy, RequestAttachment.new) }
 
 				# Comments
-				it { should be_able_to(:read, Comment) }
-				it { should be_able_to(:create, Comment) }
-				it { should be_able_to(:update, attachment) }
+				it { should     be_able_to(:read, Comment) }
+				it { should     be_able_to(:create, Comment) }
+				it { should     be_able_to(:update, attachment) }
 				it { should_not be_able_to(:update, Comment.new) }
-				it { should be_able_to(:destroy, attachment) }
+				it { should     be_able_to(:destroy, attachment) }
 				it { should_not be_able_to(:destroy, Comment.new) }
 
 				# Templates
-				it { should be_able_to(:read, Template) }
+				it { should     be_able_to(:read, Template) }
+
+				#Publication Requests
+				it { should_not be_able_to(:read, PublicationRequest.new) }
+				it { should     be_able_to(:read, PublicationRequest.new(user_id: user.id)) }
+			end
+
+			context "when is a designer" do
+				it "can read requests that they are a designer for" do
+					user.roles = [:designer, :user]
+					should_not be_able_to(:read, PublicationRequest.new)
+					should     be_able_to(:read, PublicationRequest.new(designer_id: user.id)) 
+				end
+
+				it "can design requests that they are a designer for" do
+					user.roles = [:designer, :user]
+					should_not be_able_to(:design, PublicationRequest.new)
+					should     be_able_to(:design, PublicationRequest.new(designer_id: user.id)) 
+				end
+			end
+
+			context "when is a reviewer" do
+				it "can read requests that they are a reviewer for" do
+					user.roles = [:reviewer, :user]
+					should_not be_able_to(:read, PublicationRequest.new)
+					should     be_able_to(:read, PublicationRequest.new(reviewer_id: user.id))
+				end
+
+				it "can read requests that they are a reviewer for" do
+					user.roles = [:reviewer, :user]
+					should_not be_able_to(:review, PublicationRequest.new)
+					should     be_able_to(:review, PublicationRequest.new(reviewer_id: user.id))
+				end
 			end
 
 			context "when is anonymous" do
 				let(:user) { nil }
 				it { should be_able_to(:create, :session) }
 				it { should_not be_able_to(:create, User) }
-				it { should_not be_able_to(:read, User) }
+				it { should_not be_able_to(:read,   User) }
 				it { should_not be_able_to(:update, User) }
 				it { should_not be_able_to(:delete, User) }
+
+				it { should_not be_able_to(:create, PublicationRequest) }
+				it { should_not be_able_to(:read,   PublicationRequest) }
+				it { should_not be_able_to(:update, PublicationRequest) }
+				it { should_not be_able_to(:delete, PublicationRequest) }
 			end
 		end
 	end
