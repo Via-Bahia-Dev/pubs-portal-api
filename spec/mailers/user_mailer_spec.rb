5e7e474583e715ad6f5f79c8d85ebdb08f655ad1
Nathan Connor <natyconnor@gmail.com>
2016-02-19 16:43:23 -0800

    Mailer tests

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,16 +2,17 @@ require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
   describe "password_reset" do
-    let(:mail) { UserMailer.password_reset }
+    let(:user) { User.create(email: "user@email.com", password: "af3714ff0ffae", first_name: "user", last_name: "test", password_reset_token: "abcdef0123456") }
+    let(:mail) { UserMailer.password_reset(user) }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Password reset")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.subject).to eq("Password Reset")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["publications.portal@gmail.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to match("http://localhost:3000/password_resets/abcdef0123456/edit")
     end
   end
 
